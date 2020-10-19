e92e4fd3e7b0652bc0a3a07d64de1ffdce28f86f
Seth <sethgoldin27@gmail.com>
2020-10-19 19:28:19 -0400

    remove unused login methods

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -169,8 +169,6 @@ class _EmailLinkSignInSectionState extends State<_EmailLinkSignInSection> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
 
-  String _userEmail;
-
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -194,18 +192,6 @@ class _EmailLinkSignInSectionState extends State<_EmailLinkSignInSection> {
                   return null;
                 },
               ),
-              // Container(
-              //   padding: const EdgeInsets.only(top: 16.0),
-              //   alignment: Alignment.center,
-              //   child: SignInButtonBuilder(
-              //     icon: Icons.insert_link,
-              //     text: "Sign In",
-              //     backgroundColor: Colors.blueGrey[700],
-              //     onPressed: () async {
-              //       _signInWithEmailAndLink();
-              //     },
-              //   ),
-              // ),
             ],
           ),
         )));
@@ -216,32 +202,6 @@ class _EmailLinkSignInSectionState extends State<_EmailLinkSignInSection> {
     _emailController.dispose();
     super.dispose();
   }
-
-  // void _signInWithEmailAndLink() async {
-  //   try {
-  //     _userEmail = _emailController.text;
-  //     await _auth.sendSignInLinkToEmail(
-  //         email: _userEmail,
-  //         actionCodeSettings: ActionCodeSettings(
-  //           url:
-  //               'https://react-native-firebase-testing.firebaseapp.com/emailSignin',
-  //           handleCodeInApp: true,
-  //           iOSBundleId: 'io.flutter.plugins.firebaseAuthExample',
-  //           androidPackageName: 'io.flutter.plugins.firebaseauthexample',
-  //           androidInstallApp: true,
-  //           androidMinimumVersion: "1",
-  //         ));
-
-  //     Scaffold.of(context).showSnackBar(SnackBar(
-  //       content: Text("An email has been sent to ${_userEmail}"),
-  //     ));
-  //   } catch (e) {
-  //     print(e);
-  //     Scaffold.of(context).showSnackBar(SnackBar(
-  //       content: Text("Sending email failed"),
-  //     ));
-  //   }
-  // }
 }
 
 class _OtherProvidersSignInSection extends StatefulWidget {
@@ -254,174 +214,20 @@ class _OtherProvidersSignInSection extends StatefulWidget {
 class _OtherProvidersSignInSectionState
     extends State<_OtherProvidersSignInSection> {
   final TextEditingController _tokenController = TextEditingController();
-  final TextEditingController _tokenSecretController = TextEditingController();
-
-  int _selection = 0;
-  bool _showAuthSecretTextField = false;
-  bool _showProviderTokenField = true;
-  String _provider = 'GitHub';
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: const Text('Social Authentication',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                alignment: Alignment.center,
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 16),
-                child: kIsWeb
-                    ? Text(
-                        'When using Flutter Web, API keys are configured through the Firebase Console. The below providers demonstrate how this works')
-                    : Text(
-                        'We do not provide an API to obtain the token for below providers apart from Google '
-                        'Please use a third party service to obtain token for other providers.'),
-                alignment: Alignment.center,
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 16.0),
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ListTile(
-                      title: Text('GitHub'),
-                      leading: Radio<int>(
-                        value: 0,
-                        groupValue: _selection,
-                        onChanged: _handleRadioButtonSelected,
-                      ),
-                    ),
-                    Visibility(
-                      visible: !kIsWeb,
-                      child: ListTile(
-                        title: Text('Facebook'),
-                        leading: Radio<int>(
-                          value: 1,
-                          groupValue: _selection,
-                          onChanged: _handleRadioButtonSelected,
-                        ),
-                      ),
-                    ),
-                    ListTile(
-                      title: Text('Twitter'),
-                      leading: Radio<int>(
-                        value: 2,
-                        groupValue: _selection,
-                        onChanged: _handleRadioButtonSelected,
-                      ),
-                    ),
-                    ListTile(
-                      title: Text('Google'),
-                      leading: Radio<int>(
-                        value: 3,
-                        groupValue: _selection,
-                        onChanged: _handleRadioButtonSelected,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Visibility(
-                visible: _showProviderTokenField && !kIsWeb,
-                child: TextField(
-                  controller: _tokenController,
-                  decoration: const InputDecoration(
-                      labelText: 'Enter provider\'s token'),
-                ),
-              ),
-              Visibility(
-                visible: _showAuthSecretTextField && !kIsWeb,
-                child: TextField(
-                  controller: _tokenSecretController,
-                  decoration: const InputDecoration(
-                      labelText: 'Enter provider\'s authTokenSecret'),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 16.0),
-                alignment: Alignment.center,
-                child: SignInButton(
-                  _provider == "GitHub"
-                      ? Buttons.GitHub
-                      : (_provider == "Facebook"
-                          ? Buttons.Facebook
-                          : (_provider == "Twitter"
-                              ? Buttons.Twitter
-                              : Buttons.GoogleDark)),
-                  text: "Sign In",
-                  onPressed: () async {
-                    _signInWithOtherProvider();
-                  },
-                ),
-              ),
-            ],
-          )),
+    return Column(
+      children: [
+        SignInButton(
+          Buttons.Google,
+          onPressed: () => _signInWithGoogle(),
+        ),
+        SignInButton(Buttons.GitHub, onPressed: () => _signInWithGithub())
+      ],
     );
   }
 
-  void _handleRadioButtonSelected(int value) {
-    setState(() {
-      _selection = value;
-
-      switch (_selection) {
-        case 0:
-          {
-            _provider = "GitHub";
-            _showAuthSecretTextField = false;
-            _showProviderTokenField = true;
-          }
-          break;
-
-        case 1:
-          {
-            _provider = "Facebook";
-            _showAuthSecretTextField = false;
-            _showProviderTokenField = true;
-          }
-          break;
-
-        case 2:
-          {
-            _provider = "Twitter";
-            _showAuthSecretTextField = true;
-            _showProviderTokenField = true;
-          }
-          break;
-
-        default:
-          {
-            _provider = "Google";
-            _showAuthSecretTextField = false;
-            _showProviderTokenField = false;
-          }
-      }
-    });
-  }
-
-  void _signInWithOtherProvider() {
-    switch (_selection) {
-      case 0:
-        _signInWithGithub();
-        break;
-      case 1:
-        _signInWithFacebook();
-        break;
-      case 2:
-        _signInWithTwitter();
-        break;
-      default:
-        _signInWithGoogle();
-    }
-  }
-
-  // Example code of how to sign in with Github.
   void _signInWithGithub() async {
     try {
       UserCredential userCredential;
@@ -448,54 +254,6 @@ class _OtherProvidersSignInSectionState
     }
   }
 
-  // Example code of how to sign in with Facebook.
-  void _signInWithFacebook() async {
-    try {
-      final AuthCredential credential = FacebookAuthProvider.credential(
-        _tokenController.text,
-      );
-      final User user = (await _auth.signInWithCredential(credential)).user;
-
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text("Sign In ${user.uid} with Facebook"),
-      ));
-    } catch (e) {
-      print(e);
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text("Failed to sign in with Facebook: ${e}"),
-      ));
-    }
-  }
-
-  // Example code of how to sign in with Twitter.
-  void _signInWithTwitter() async {
-    try {
-      UserCredential userCredential;
-
-      if (kIsWeb) {
-        TwitterAuthProvider twitterProvider = TwitterAuthProvider();
-        await _auth.signInWithPopup(twitterProvider);
-      } else {
-        final AuthCredential credential = TwitterAuthProvider.credential(
-            accessToken: _tokenController.text,
-            secret: _tokenSecretController.text);
-        userCredential = await _auth.signInWithCredential(credential);
-      }
-
-      final user = userCredential.user;
-
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text("Sign In ${user.uid} with Twitter"),
-      ));
-    } catch (e) {
-      print(e);
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text("Failed to sign in with Twitter: ${e}"),
-      ));
-    }
-  }
-
-  //Example code of how to sign in with Google.
   void _signInWithGoogle() async {
     try {
       UserCredential userCredential;
